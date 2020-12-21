a6186afee7deb18a1617a297cd55dbf68e050371
釼持智昭 <t.kennmochi.1985@gmail.com>
2020-12-21 13:21:59 +0900

    ５箇所に保存して平均値を出せるように

--- main.js
+++ main.js
@@ -35,23 +35,32 @@ document.getElementsByTagName("button")[0].addEventListener("click" , ()=> {
 
 // タイマー部分
 
-// 以下丸パクリ 
+// 以下丸パクリ
 // 性質上、何時間は不要なので、時間関連は削除。
 
 const timeElement = document.getElementById('time');
+
 const start = document.getElementById('start');
+
 const stop = document.getElementById('stop');
+
 const reset = document.getElementById('reset');
 
-const record = document.getElementById("record");
+const record1 = document.getElementById("record1");
+const record2 = document.getElementById("record2");
+const record3 = document.getElementById("record3");
+const record4 = document.getElementById("record4");
+const record5 = document.getElementById("record5");
+
+// console.log(counter)
 
 // console.log(document.getElementById("record").innerText);
 
 // 経過時間のミリ秒
+
 let elapsed = 0;
 
 let intervalId = null;
-
 
 function updateTime() {
   const ms = elapsed % 1000;
@@ -78,29 +87,76 @@ start.addEventListener('click', function(e) {
   }, 10);
 });
 
-stop.addEventListener('click', function(e) {
 
-    // 
-    const ms = elapsed % 1000;
-    const s = Math.floor(elapsed / 1000) % 60;
-    const m = Math.floor(elapsed / (1000*60)) % 60;
-    const h = Math.floor(elapsed / (1000*60*60));
-  
-    const msStr = ms.toString().padStart(3, '0');
-    const sStr = s.toString().padStart(2, '0');
-    const mStr = m.toString().padStart(2, '0');
-    const hStr = h.toString().padStart(2, '0');
+// ストップボタン押した回数
+let not = 0;
+function counter(){
 
-    // function updateTime下で定数指定をしている為、同じように表示するにはここにも必要。
-    // 
+  const ms = elapsed % 1000;
+  const s = Math.floor(elapsed / 1000) % 60;
+  const m = Math.floor(elapsed / (1000*60)) % 60;
+  const h = Math.floor(elapsed / (1000*60*60));
+
+  const msStr = ms.toString().padStart(3, '0');
+  const sStr = s.toString().padStart(2, '0');
+  const mStr = m.toString().padStart(2, '0');
+  const hStr = h.toString().padStart(2, '0');
+
+  not++;
+  document.getElementById("stop_counter").innerHTML = not;
 
 
-    clearInterval(intervalId);
-    intervalId = null;
-    record.innerHTML = `${mStr}:${sStr}.${msStr}`;
-});
+  if (not == 1) {
+    record1.innerHTML = `${mStr}:${sStr}.${msStr}`;
+    elapsed1 = elapsed;
+  }
+  else if (not == 2) {
+    record2.innerHTML = `${mStr}:${sStr}.${msStr}`;
+    elapsed2 = elapsed;
+  }
+  else if (not == 3) {
+    record3.innerHTML = `${mStr}:${sStr}.${msStr}`;
+    elapsed3 = elapsed;
+  }
+  else if (not == 4) {
+    record4.innerHTML = `${mStr}:${sStr}.${msStr}`;
+    elapsed4 = elapsed;
+  }
+  else if (not == 5){
+    record5.innerHTML = `${mStr}:${sStr}.${msStr}`;
+    elapsed5 = elapsed;
+
+    average = [elapsed1 + elapsed2 + elapsed3  + elapsed4 + elapsed5]/5;
+
+    const a_ms = average % 1000;
+    const a_s = Math.floor(average / 1000) % 60;
+    const a_m = Math.floor(average / (1000*60)) % 60;
+    const a_h = Math.floor(average / (1000*60*60));
+
+    const a_msStr = a_ms.toString().padStart(3, '0');
+    const a_sStr = a_s.toString().padStart(2, '0');
+    const a_mStr = a_m.toString().padStart(2, '0');
+    const a_hStr = a_h.toString().padStart(2, '0');
+
+    average_record.innerHTML = `${a_mStr}:${a_sStr}.${a_msStr}`;
+  };
+
+  clearInterval(intervalId);
+  intervalId = null;
+
+};
 
 reset.addEventListener('click', function(e) {
   elapsed = 0;
+
+  // ラップタイムの場合
+  // not = 0;
+  // document.getElementById("stop_counter").innerHTML = not;
+  // record1.innerHTML = "00:00.000"
+  // record2.innerHTML = "00:00.000"
+  // record3.innerHTML = "00:00.000"
+  // record4.innerHTML = "00:00.000"
+  // record5.innerHTML = "00:00.000"
+
   updateTime();
 });
